6068b2815a9f1dce99d57be69bf9d621f946e9ec
Jakub <jsapalawebdev@gmail.com>
2023-11-01 18:14:51 +0100

    Initial commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'shared/components/ui/Button'
+
+export default function Page() {
+  return (
+    <>
+      <h1>Web</h1>
+      <Button variant='outline'>Click me</Button>
+    </>
+  );
+}
---
+++ packages/shared/index.ts
@@ -0,0 +1,6 @@
+// @ts-expect-error
+export * from "./components";
+// @ts-expect-error
+export * from "./styles";
+// @ts-expect-error
+export * from "./lib";
